73421ad0ed23b5e5ab7b0c0764cb9cd66b3f11ec
Bonnie Bright <blbright@swbell.net>
2016-05-03 09:49:21 -0500

    initial commit

---
+++ db/migrate/20160502183514_create_checkins.rb
@@ -0,0 +1,9 @@
+class CreateCheckins < ActiveRecord::Migration
+  def change
+    create_table :checkins do |t|
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
