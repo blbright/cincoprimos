1bb58496d27c4855dd6ff88fe3054dc5cf71c91a
Bonnie Bright <blbright@swbell.net>
2016-05-31 13:32:10 -0500

    db problem fixedgit add . git add .

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,24 @@
 class CommentsController < ApplicationController
   def new
-    @comment = Comment.new
     @reservation = Reservation.find(params[:reservation_id])
+    @comment = Comment.new
+
   end
 
   def create
-    @comment = Comment.new
     @reservation = Reservation.find(params[:reservation_id])
+    @comment = @reservation.comments.create(comment_params)
+
 
     @comment.content = params[:comment][:content]
-    #@comment.reservation = @reservation
+    @comment.reservation = @reservation
     @comment.save
 
     redirect_to reservation_path(@reservation)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :reservation_id)
   end
 end
